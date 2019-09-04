8f308bfaa0e3e3e0439913c09d96178864b32930
Bryan <bsantelicescgmail.com>
2019-09-04 16:09:48 -0400

    Change of distribution

---
+++ src/main/java/mingeso/mingeso/MingesoApplication.java
@@ -0,0 +1,13 @@
+package mingeso.mingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingesoApplication.class, args);
+	}
+
+}

6cb23d818d1455fde437226c61375cec53e078c4
Unknown <jorge.plaza@usach.cl>
2019-10-09 12:21:00 -0300

    Unit Test for Role and Room

---
+++ src/test/java/mingeso/mingeso/RoleTest.java
@@ -0,0 +1,39 @@
+package mingeso.mingeso;
+
+import mingeso.mingeso.models.Role;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class RoleTest {
+
+    private Role role;
+
+    @BeforeEach
+    public void initializeRole() {
+        Role role = new Role();
+        long roleId = 5;
+        role.setRoleId(roleId);
+        role.setType(0);
+    }
+
+    @Test
+    @DisplayName("Test for get role Id")
+    public void getRoleIdTest() {
+        long roleId = 0;
+        long realRoleId = role.getRoleId()
+        Assertions.assertEquals(0, realRoleId);
+    }
+
+    @Test
+    @DisplayName("Test for get role type")
+    public void getRoleTypeTest() {
+        Assertions.assertEquals(0, role.getType());
+    }
+}
--- src/test/java/mingeso/mingeso/RoomTest.java
+++ src/test/java/mingeso/mingeso/RoomTest.java
@@ -18,10 +18,14 @@ public class RoomTest {
     @BeforeEach
     public void initializeRoom() {
         room = new Room();
+        long roomId = 7;
+        room.setRoomId(roomId);
         room.setAdultCapacity(5);
         room.setChildCapacity(5);
         room.setPrice(3000);
         room.setType(2);
+        room.setRoomNumber(101);
+        room.setImageLink("https://hotelvarunamanizales.com/wp-content/uploads/2016/02/habitacion-superior-sencilla-hotel-manizales-varuna-4.jpg");
     }
 
     @Test
@@ -34,5 +38,38 @@ public class RoomTest {
     @DisplayName("Test for get Adult capacity method")
     public void adultGetCapacityTest() {
         Assertions.assertEquals(5, room.getAdultCapacity());
+    }
+
+    @Test
+    @DisplayName("Test for get price of room")
+    public void getPriceTest() {
+        Assertions.assertEquals(3000, room.getPrice());
+    }
+
+    @Test
+    @DisplayName("Test for get room number")
+    public void getRoomNumberTest() {
+        Assertions.assertEquals(101, room.getRoomNumber());
+    }
+
+    @Test
+    @DisplayName("Test for get room type")
+    public void getRoomTypeTest() {
+        Assertions.assertEquals(2, room.getType());
+    }
+
+    @Test
+    @DisplayName("Test for get image link")
+    public void getImageLinkTest() {
+        Assertions.assertEquals("https://hotelvarunamanizales.com/wp-content/uploads/2016/02/habitacion-superior-sencilla-hotel-manizales-varuna-4.jpg",
+                                room.getImageLink());
+    }
+
+    @Test
+    @DisplayName("Test for get room id")
+    public void getRoomIdTest() {
+        long roomId = 7;
+        long realRoomId = room.getRoomId();
+        Assertions.assertEquals(roomId, realRoomId);
     }
 }

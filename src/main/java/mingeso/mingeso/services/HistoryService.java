5e22f2e86e24471a23935ee42923d62e79529013
Matias <matias.coronado@usach.cl>
2019-10-09 15:34:45 -0300

    Fix some creation method's
    - History
    - Reservation

--- src/main/java/mingeso/mingeso/services/HistoryService.java
+++ src/main/java/mingeso/mingeso/services/HistoryService.java
@@ -1,7 +1,9 @@
 package mingeso.mingeso.services;
 
 import mingeso.mingeso.models.History;
+import mingeso.mingeso.models.User;
 import mingeso.mingeso.repositories.HistoryRepository;
+import mingeso.mingeso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,8 @@ public class HistoryService {
 
     @Autowired
     private HistoryRepository historyRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping(value = "/histories")
     @ResponseBody
@@ -29,6 +33,13 @@ public class HistoryService {
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public ResponseEntity create(@RequestBody History history) {
-        return new ResponseEntity(historyRepository.save(history), HttpStatus.CREATED);
+        if(null == userRepository.findById(history.getUser().getUserId())){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);}
+        else{
+            History newHistory = new History();
+            newHistory.setAditionalInfo(history.getAditionalInfo());
+            newHistory.setUser(history.getUser());
+            return new ResponseEntity(historyRepository.save(history), HttpStatus.CREATED);
+        }
     }
 }
--- src/main/java/mingeso/mingeso/services/ReservationService.java
+++ src/main/java/mingeso/mingeso/services/ReservationService.java
@@ -1,7 +1,11 @@
 package mingeso.mingeso.services;
 
 import mingeso.mingeso.models.Reservation;
+import mingeso.mingeso.models.Room;
+import mingeso.mingeso.models.User;
 import mingeso.mingeso.repositories.ReservationRepository;
+import mingeso.mingeso.repositories.RoomRepository;
+import mingeso.mingeso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +24,12 @@ public class ReservationService {
     @Autowired
     ReservationRepository reservationRepository;
 
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping(value = "/reservations")
     @ResponseBody
     public ResponseEntity getAll() {
@@ -30,6 +40,31 @@ public class ReservationService {
     @PostMapping(value = "/create")
     @ResponseBody
     public ResponseEntity create(@RequestBody Reservation reservation) {
-        return new ResponseEntity(reservationRepository.save(reservation), HttpStatus.CREATED);
+
+        List<Room> inputRoomList = reservation.getRoomList();
+        User inputUser = reservation.getUser();
+        Boolean verification = true;
+
+        for(int i = 0; i < inputRoomList.size();i++) {
+            if(null==roomRepository.findById(inputRoomList.get(i).getRoomId())){
+                verification = false;
+                break;
+            }
+        }
+        if(null == userRepository.findById(reservation.getUser().getUserId())){
+            verification = false;
+        }
+
+        if(verification == false) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }else{
+            Reservation newReservation = new Reservation();
+            newReservation.setEstate(reservation.getEstate());
+            newReservation.setFinalDate(reservation.getFinalDate());
+            newReservation.setInitialDate(reservation.getInitialDate());
+            newReservation.setRoomList(reservation.getRoomList());
+            newReservation.setUser(reservation.getUser());
+            return new ResponseEntity(reservationRepository.save(newReservation), HttpStatus.CREATED);
+        }
     }
 }
